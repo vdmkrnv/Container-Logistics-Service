a7241b7f34bdd7fcabb0f8810bf15d06b191a349
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-05-27 10:20:36 +0300

    add Dockerfile and docker-compose.yml

---
+++ OrderService/Infrastructure/Migrations/20240521105343_AddIsCanceledProperty.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCanceledProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_deleted",
+                table: "orders",
+                newName: "is_canceled");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "client_id",
+                table: "orders",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "client_id",
+                table: "orders");
+
+            migrationBuilder.RenameColumn(
+                name: "is_canceled",
+                table: "orders",
+                newName: "is_deleted");
+        }
+    }
+}
