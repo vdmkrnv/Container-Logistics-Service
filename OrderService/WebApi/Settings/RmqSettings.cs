ff462c387fb4af15ee4888cf4a5863db1fa8a1f1
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-06-04 10:56:42 +0300

    update docker-compose.yml add rabbitmq to docker-compose.yml

---
+++ OrderService/WebApi/Settings/RmqSettings.cs
@@ -0,0 +1,12 @@
+namespace WebApi.Settings;
+
+public class RmqSettings
+{
+     string Host { get; set; }
+    
+     string VHost { get; set; }
+    
+     string Login { get; set; }
+    
+     string Password { get; set; }
+}
