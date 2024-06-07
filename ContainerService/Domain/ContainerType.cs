ad86ec07d38e671d6a201de3ea7dec8f8b37df5c
Nikita Berezhko <berezhko.nik16@gmail.com>
2024-06-07 08:48:37 +0300

    add migrations, Dockerfile and docker-compose.yml

--- ContainerService/Domain/ContainerType.cs
+++ ContainerService/Domain/ContainerType.cs
@@ -1,8 +1,17 @@
 ﻿namespace Domain;
 
+/// <summary>
+/// Доменная сущность контейнера
+/// </summary>
 public class ContainerType : BaseEntity
 {
+	/// <summary>
+	/// Название типа
+	/// </summary>
 	public string? Name { get; set; }
 
+	/// <summary>
+	///  Цена за день аренды
+	/// </summary>
 	public double PricePerDay { get; set; }
 }
---
+++ ContainerService/Infrastructure/Migrations/20240604131821_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContainerType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    PricePerDay = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContainerType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsoNumber = table.Column<string>(type: "text", nullable: false),
+                    ContainerTypeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsEngaged = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Containers_ContainerType_ContainerTypeId",
+                        column: x => x.ContainerTypeId,
+                        principalTable: "ContainerType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Containers_ContainerTypeId",
+                table: "Containers",
+                column: "ContainerTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Containers");
+
+            migrationBuilder.DropTable(
+                name: "ContainerType");
+        }
+    }
+}
